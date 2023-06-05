d0adce195e17a35fef413f993b507ee39b0cadb8
BJaudun <108735550+BJaudun@users.noreply.github.com>
2023-06-05 10:18:14 +0100

    Add files via upload

---
+++ energy_minimisation_final.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#requires access to em.mdp file
+
+
+pdb2pqr30 --ff CHARMM  --keep-chain  $2_refined.pdb pqr.pdb
+
+gmx pdb2gmx -f pqr.pdb -ff charmm27 -ignh true -water tip3p -o conf.pdb
+
+gmx editconf -f conf.pdb -d 8 -c true -o conf.pdb
+
+gmx grompp -f em_100.mdp -maxwarn 5  -o em -c conf.pdb #requires em.mdp 
+
+gmx mdrun -deffnm em -c clean.pdb
+
+echo 'system' | gmx trjconv -f clean.pdb  -o mpm_refine_$1  -s em.tpr
